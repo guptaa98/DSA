12474efdf25de3bab72c9137cd61cefb222b3b52
Prashasti Gupta <prashasti708@gmail.com>
2021-03-26 01:07:47 +0530

    Update Pacific Atlantic Water Flow.cpp

--- Leetcode/March Challenge/Pacific Atlantic Water Flow.cpp
+++ Leetcode/March Challenge/Pacific Atlantic Water Flow.cpp
@@ -1,20 +1,11 @@
-
 class Solution {
 public:
- 
-	// TIME COMPLEXITY:- O(N*M)
-	// SPACE COMPLEXITY:- O(N*M)
- 
-    #define F first
-    #define S second
-    #define pii pair<int,int>
- 
-    void get_ans_by_bfs(int m,int n,queue<pii>& q,vector<vector<bool>>& vis,vector<vector<int>>& grid)
+    void getwaterflow(int m, int n, queue<pair<int,int>>&q ,vector<vector<bool>>&vis,vector<vector<int>>& grid)
     {
         while(!q.empty())
         {
-            int x = q.front().F;
-            int y = q.front().S;
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
  
             if(x+1<m and !vis[x+1][y] and grid[x][y]<=grid[x+1][y])
@@ -41,26 +32,25 @@ public:
             }
         }
     }
- 
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix)
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) 
     {
- 
         vector<vector<int>> ans;
- 
         int m = matrix.size();
- 
         // base cases
         if(m==0)
+        {
             return ans;
- 
+        }
         int n = matrix[0].size();
         if(n==0)
+        {
             return ans;
- 
+        }
+        
         vector<vector<bool>> pacific(m,vector<bool>(n,false));
         vector<vector<bool>> atlantic(m,vector<bool>(n,false));
  
-        queue<pii> q;
+        queue<pair<int,int>> q;
  
         // do for pacific ocean
  
@@ -76,27 +66,21 @@ public:
             pacific[0][i] = true;
         }
  
-        get_ans_by_bfs(m,n,q,pacific,matrix);
- 
+        getwaterflow(m,n,q,pacific,matrix);
         // do for atlantic ocean
- 
         for(int i=0;i<m;i++)
         {
             q.push({i,n-1});
             atlantic[i][n-1] = true;
         }
- 
         for(int i=0;i<n;i++)
         {
             q.push({m-1,i});
             atlantic[m-1][i] = true;
         }
- 
-        get_ans_by_bfs(m,n,q,atlantic,matrix);
- 
+        getwaterflow(m,n,q,atlantic,matrix);
         // push the cells where water can go to both the oceans
- 
-        for(int i=0;i<m;i++)
+        for(int i=0 ; i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
@@ -104,7 +88,6 @@ public:
                     ans.push_back({i,j});
             }
         }
- 
-        return ans;      
+        return ans;
     }
 };
